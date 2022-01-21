c71553844f61a94ff7dbad9224fafd0879c09c13
Jonathan <djgrovezone@gmail.com>
2022-01-21 18:32:03 +0100

    cleanup

---
+++ dist/Firebase-Invitados/assets/grupos.ts
@@ -0,0 +1,39 @@
+export const Grupos = [
+    {
+        id: 0,
+        descripcion: "Sin Grupo",
+    },
+    {
+        id: 1,
+        descripcion: "Moya Moreno",
+    },
+    {
+        id: 2,
+        descripcion: "Garrido Maldonado",
+    },
+    {
+        id: 3,
+        descripcion: "Moya Villarejo",
+    },
+    {
+        id: 4,
+        descripcion: "Moya Garcia",
+    },
+    {
+        id: 5,
+        descripcion: "Moya De La Chica",
+    },
+    {
+        id: 6,
+        descripcion: "Garrido Chicharro",
+    },
+    {
+        id: 7,
+        descripcion: "Moreno Rusillo",
+    },
+    {
+        id: 8,
+        descripcion: "Moreno Fernández",
+    },
+
+]; 
